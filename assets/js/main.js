2380fb8cc6a379de06724617137b9d86a78d1b29
medrayentn <medrayenelmi@gmail.com>
2025-07-19 16:55:07 +0100

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -48,13 +48,12 @@ const sr = ScrollReveal({
     distance: '60px',
     duration: 2000,
     delay: 200,
-//     reset: true
 });
 
 sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text, .services__title', {});
-sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img', { delay: 400 });
+sr.reveal('.home__img, .about__subtitle, .about__text', { delay: 400 });
 sr.reveal('.home__social-icon', { interval: 200 });
-sr.reveal('.skills__data, .work__img, .contact__input, .services__card', { interval: 200 });
+sr.reveal('.skills__circle, .work__img, .contact__input, .services__card', { interval: 200 });
 
 
 /* Magnific Popup Initialization */
